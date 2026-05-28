8337901b5a57ceefc8c2746e87903c1b8526b02e
liuye <liuye@cardioacc.com>
2026-05-28 17:29:00 +0800

    提交

--- MyMessage/src/utils/util.cpp
+++ MyMessage/src/utils/util.cpp
@@ -1,6 +1,16 @@
 #include "util.h"
 
+
 void Util::SplitStr(const QString &s, QStringList &v, const QString &c)
 {
+    if (s.isEmpty() || c.isEmpty())
+        return;
 
+    for (auto ca : s)
+    {
+        if (ca == c)
+        {
+
+        }
+    }
 }
